108a1a54b6664d79157490e95c26206437a79d32
f-bouwmeester <fbouwmeester1994@gmail.com>
2024-01-17 17:27:13 +0100

    addings

---
+++ quasar-project/src/pages/IndexPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page class="flex flex-center column">
+    <div class="row">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+    <q-spinner-ball
+          color="primary"
+          size="2em"
+        />
+        <q-btn
+          :loading="isLoading"
+          label="bouwvakker"
+          @click="hatsenflats"
+        />
+      </div>
+    <div class="row fit"> 
+      <div class="col-12">x</div>
+      <div class="col-6">x</div>
+      <div class="col-6">x</div>
+    </div>
+
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+     const isLoading = ref(false)
+    const hatsenflats = () => {
+      isLoading.value = !isLoading.value
+    }
+</script>
